62ab2d761f29402ff6c7834f8ba3880acf62d0ce
Shivam Vatshayan <vatshayanshiv1998@gmail.com>
2022-03-24 19:56:02 +0530

    Create image.sol

---
+++ image.sol
@@ -0,0 +1,13 @@
+// This is just a demo for full working project please mail me at ( vatshayan007@gmail.com )
+
+
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.10 and less than 0.9.0
+pragma solidity ^0.8.10;
+
+contract HelloWorld {
+    string public greet = "Hello World!";
+}
+
+// Contact : vatshayan007@gmail.com 
+
